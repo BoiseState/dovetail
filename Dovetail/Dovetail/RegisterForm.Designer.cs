d4f00f6d0b2675d6075b580f087eefd8196c5b97
NarthVader <nathanberkenmeier@u.boisestate.edu>
2018-09-25 20:52:11 -0600

    just need to do proper insert

--- Dovetail/Dovetail/RegisterForm.Designer.cs
+++ Dovetail/Dovetail/RegisterForm.Designer.cs
@@ -28,11 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Dovetail.Properties.Resources.Logo2;
+            this.pictureBox1.Location = new System.Drawing.Point(166, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(535, 365);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(100, 621);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label2.Location = new System.Drawing.Point(107, 754);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(373, 623);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(424, 38);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Please Enter a Valid Username";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(373, 756);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(424, 38);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Please Enter a Valid Password";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.Location = new System.Drawing.Point(314, 862);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 90);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(867, 1003);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "RegisterForm";
             this.Text = "RegisterForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Dovetail/Dovetail/RegisterForm.cs
+++ Dovetail/Dovetail/RegisterForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,63 @@ namespace Dovetail
         public RegisterForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // SQL Server database connection information
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "dovetail2018.database.windows.net";
+            builder.UserID = "admin0";
+            builder.Password = "bronco!devs098";
+            builder.InitialCatalog = "dovetail-db";
+
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+
+            // Attempt to connect to database and verify user credentials
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                MessageBox.Show("fuck ya, we're connected. dick to pussy");
+                this.Close();
+                // Check username/password/access query
+                //StringBuilder sb = new StringBuilder();
+                //sb.Append("INSERT COUNT(1) ");
+                //sb.Append("FROM Users ");
+                //sb.Append("WHERE Username = @Username AND Password = @Password AND Access = 1;");
+                //String sql = sb.ToString();
+
+                //SqlCommand command = new SqlCommand(sql, connection);
+                //command.CommandType = CommandType.Text;
+                //command.Parameters.AddWithValue("@Username", txtUsername.Text);
+                //command.Parameters.AddWithValue("@Password", txtPassword.Password);
+
+                //int count = Convert.ToInt32(command.ExecuteScalar());
+                //if (count == 1)
+                //{
+                //   // Show main dashboard window (TODO: show userType-specific window)
+                //    DovetailMainForm dashboard = new DovetailMainForm();
+                //    dashboard.Show();
+                //    Close();    // close sign in window
+                //}
+                //else
+                //{
+                //    txtUsername.Clear();
+                //    txtPassword.Clear();
+                //   MessageBox.Show("Username or password is incorrect!");  // may want to show error message text in-window, not popup
+                //}
+            }
+            catch (SqlException sqle)
+            {
+                MessageBox.Show(sqle.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
