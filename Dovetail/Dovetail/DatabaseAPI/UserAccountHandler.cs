c022d948f198da0c92a89acb3a53d409e303c3f4
Justin Brown <justin.brown15@outlook.com>
2018-09-29 01:28:25 -0600

    Started database API; simplified sign-in logic

---
+++ Dovetail/Dovetail/DatabaseAPI/UserAccountHandler.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dovetail.DatabaseAPI
+{
+    public static class UserAccountHandler
+    {
+        private static SqlConnection GetDovetailDbConnection()
+        {
+            // SQL Server database connection information
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "dovetail2018.database.windows.net";
+            builder.UserID = "admin0"; 
+            builder.Password = "bronco!devs098";
+            builder.InitialCatalog = "dovetail-db";
+
+            return new SqlConnection(builder.ConnectionString);
+        }
+
+        public static bool UserSignIn(string username, string password)
+        {
+            // Prepare connection to the database
+            SqlConnection connection = GetDovetailDbConnection();
+            bool canSignIn = false;
+
+            // Attempt to connect to database and verify user credentials
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                // Prepare sign-in query
+                StringBuilder sb = new StringBuilder();
+                sb.Append("SELECT COUNT(1) ");
+                sb.Append("FROM Users ");
+                sb.Append("WHERE Username = @Username AND Password = @Password AND Access = 1;");
+                string sql = sb.ToString();
+
+                SqlCommand command = new SqlCommand(sql, connection)
+                {
+                    CommandType = CommandType.Text
+                };
+
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    connection.Close();
+                    return false;
+                }
+
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+
+                // Execute query; only one valid user should be found/returned
+                int queryResult = Convert.ToInt32(command.ExecuteScalar());
+                if (queryResult != 1)
+                {
+                    connection.Close();
+                    return false;
+                }
+
+                // Sign-in is successful
+                canSignIn = true;
+            }
+            catch (SqlException sqle)
+            {
+                connection.Close();
+                MessageBox.Show(sqle.Message);
+            }
+
+            connection.Close();
+
+            return canSignIn;
+        }
+
+        public static bool UserSignOut(string username)
+        {
+            return false;
+        }
+        
+        public static bool RegisterNewUser(string username, string password, string firstName, string lastName, string email, string userType, int hasAccess)
+        {
+            return false;
+        }
+
+        public static void RevokeAccessForUser(string username)
+        {
+
+        }
+
+        public static void AllowAccessForUser(string username)
+        {
+
+        }
+
+        public static void UserUpdatePassword(string oldPass, string newPass)
+        {
+
+        }
+
+        public static void UserUpdateWage(double newWage)
+        {
+
+        }
+
+        public static void UserUpdateEmail(string newEmail)
+        {
+
+        }
+    }
+}
--- Dovetail/Dovetail/SignInWindow.xaml.cs
+++ Dovetail/Dovetail/SignInWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Dovetail.DatabaseAPI;
 
 namespace Dovetail
 {
@@ -65,69 +66,20 @@ namespace Dovetail
 
         private void Sign_In()
         {
-            // SQL Server database connection information
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "dovetail2018.database.windows.net";
-            builder.UserID = "admin0";
-            builder.Password = "bronco!devs098";
-            builder.InitialCatalog = "dovetail-db";
+            bool signInSuccess = UserAccountHandler.UserSignIn(txtUsername.Text, txtPassword.Password);
 
-            SqlConnection connection = new SqlConnection(builder.ConnectionString);
-
-            // Attempt to connect to database and verify user credentials
-            try
+            if (!signInSuccess)
             {
-                if (connection.State == ConnectionState.Closed)
-                {
-                    connection.Open();
-                }
-                // Check username/password/access query
-                StringBuilder sb = new StringBuilder();
-                sb.Append("SELECT COUNT(1) ");
-                sb.Append("FROM Users ");
-                sb.Append("WHERE Username = @Username AND Password = @Password AND Access = 1;");
-                String sql = sb.ToString();
-
-                SqlCommand command = new SqlCommand(sql, connection);
-                command.CommandType = CommandType.Text;
-                if (!string.IsNullOrEmpty(txtUsername.Text) && !string.IsNullOrEmpty(txtPassword.Password))
-                {
-                    command.Parameters.AddWithValue("@Username", txtUsername.Text);
-                    command.Parameters.AddWithValue("@Password", txtPassword.Password);
-                }
-                else
-                {
-                    // Username and/or password were not present. Simply bail out and alert the user.
-                    txtUsername.Clear();
-                    txtPassword.Clear();
-                    SignInAlertMsgLabel.Content = "Incomplete sign-in credentials!";
-                    return;
-                }
-
-                int count = Convert.ToInt32(command.ExecuteScalar());
-                if (count == 1)
-                {
-                    // Show main dashboard window (TODO: show userType-specific window)
-                    DovetailMainForm dashboard = new DovetailMainForm();
-                    dashboard.Show();
-                    Close();    // close sign in window
-                }
-                else
-                {
-                    txtUsername.Clear();
-                    txtPassword.Clear();
-                    SignInAlertMsgLabel.Content = "Username or password is incorrect!";
-                }
-            }
-            catch (SqlException sqle)
-            {
+                txtUsername.Clear();
                 txtPassword.Clear();
-                MessageBox.Show(sqle.Message);
+                SignInAlertMsgLabel.Content = "Username or password is incorrect!";
+                return;
             }
-            finally
-            {
-                connection.Close();
-            }
+
+            // Show main dashboard window
+            DovetailMainForm dashboard = new DovetailMainForm();
+            dashboard.Show();
+            Close();    // close sign in window
         }
     }
 }
